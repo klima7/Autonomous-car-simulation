256abfe4dd0dc8fe14416f2123d5e3ff0fb39fe6
1mbir <mikoimb99@gmail.com>
2021-03-26 18:50:21 +0100

    Dodano funkcje do obsługi sterowania Ackermana

--- app/car.py
+++ app/car.py
@@ -13,6 +13,10 @@ class Car:
         _, self.back_left_wheel = self.client.simxGetObjectHandle('BackLeftWheelJoint', client.simxServiceCall())
         _, self.back_right_wheel = self.client.simxGetObjectHandle('BackRightWheelJoint', client.simxServiceCall())
         _, self.camera = self.client.simxGetObjectHandle('CarVision1', client.simxServiceCall())
+        _, self.body = self.client.simxGetObjectHandle('CarBody', client.simxServiceCall())
+        self.width = self.count_distance(self.front_left_wheel, self.front_right_wheel)
+        self.length = self.count_distance(self.front_left_wheel, self.back_left_wheel)
+
 
     def set_force(self, force):
         self.client.simxSetJointMaxForce(self.back_left_wheel, force, self.client.simxDefaultPublisher())
@@ -21,6 +25,14 @@ class Car:
     def set_angle(self, angle):
         self.client.simxSetJointTargetPosition(self.front_left_wheel, self.to_radians(angle), self.client.simxDefaultPublisher())
         self.client.simxSetJointTargetPosition(self.front_right_wheel, self.to_radians(angle), self.client.simxDefaultPublisher())
+
+    def set_right_angle(self, angle):
+        self.client.simxSetJointTargetPosition(self.front_right_wheel, angle,
+                                               self.client.simxDefaultPublisher())
+
+    def set_left_angle(self, angle):
+        self.client.simxSetJointTargetPosition(self.front_left_wheel, angle,
+                                               self.client.simxDefaultPublisher())
 
     def set_velocity(self, velocity):
         self.client.simxSetJointTargetVelocity(self.back_left_wheel, velocity, self.client.simxDefaultPublisher())
@@ -32,6 +44,14 @@ class Car:
         image = np.array(data, dtype=np.uint8)
         image.resize([size[1], size[0], 3])
         return image
+
+    def count_vector_distance(self, vector1, vector2):
+        return math.sqrt(((vector2[0]-vector1[0])**2)+((vector2[1]-vector1[1])**2)+((vector2[1]-vector1[1])**2))
+
+    def count_distance(self, obj1, obj2):
+        _, left_position = self.client.simxGetObjectPosition(obj1, -1, self.client.simxServiceCall())
+        _, right_position = self.client.simxGetObjectPosition(obj2, -1, self.client.simxServiceCall())
+        return self.count_vector_distance(left_position, right_position)
 
     @staticmethod
     def to_radians(degrees):
--- app/controller.py
+++ app/controller.py
@@ -8,7 +8,9 @@ class Controller:
         self.car = car
 
     def start(self):
-        self.car.set_angle(0)
+        self.car.set_angle(90)
+        self.car.set_left_angle(self.close_ackerman_angle(0.1, self.car))
+        self.car.set_right_angle(self.far_ackerman_angle(0.1, self.car))
         self.car.set_velocity(20)
         self.car.set_force(60)
 
@@ -23,3 +25,9 @@ class Controller:
         time.sleep(3)
         self.car.set_velocity(0)
         time.sleep(2)
+
+    def close_ackerman_angle(self, r, vehicle): # r - promien zakretu
+        return vehicle.length/(r - (vehicle.width/2))
+
+    def far_ackerman_angle(self, r, vehicle): #r - promien zakretu
+        return vehicle.length / (r + (vehicle.width / 2))
