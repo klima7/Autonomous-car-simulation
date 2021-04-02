4992120b0728260be8227c9d74d91e941999fc27
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-04-02 10:17:06 +0200

    Dodano kod sterujący światłami w Pythonie - I dodano program je testujący

--- app/car.py
+++ app/car.py
@@ -9,22 +9,22 @@ class Car:
     def __init__(self, client: RemoteApiClient):
         self.client = client
 
-        _, self.front_left_wheel = self.client.simxGetObjectHandle('FLSteerJoint', client.simxServiceCall())
-        _, self.front_right_wheel = self.client.simxGetObjectHandle('FRSteerJoint', client.simxServiceCall())
-        _, self.back_left_wheel = self.client.simxGetObjectHandle('BLWheelJoint', client.simxServiceCall())
-        _, self.back_right_wheel = self.client.simxGetObjectHandle('BRWheelJoint', client.simxServiceCall())
+        _, self.fl_wheel = self.client.simxGetObjectHandle('FLSteerJoint', client.simxServiceCall())
+        _, self.fr_wheel = self.client.simxGetObjectHandle('FRSteerJoint', client.simxServiceCall())
+        _, self.bl_wheel = self.client.simxGetObjectHandle('BLWheelJoint', client.simxServiceCall())
+        _, self.br_wheel = self.client.simxGetObjectHandle('BRWheelJoint', client.simxServiceCall())
         _, self.camera = self.client.simxGetObjectHandle('CarVision', client.simxServiceCall())
 
-        self.width = util.calc_distance(self.client, self.front_left_wheel, self.front_right_wheel)
-        self.length = util.calc_distance(self.client, self.front_left_wheel, self.back_left_wheel)
+        self.width = util.calc_distance(self.client, self.fl_wheel, self.fr_wheel)
+        self.length = util.calc_distance(self.client, self.fl_wheel, self.bl_wheel)
 
     def set_force(self, force):
-        self.client.simxSetJointMaxForce(self.back_left_wheel, force, self.client.simxDefaultPublisher())
-        self.client.simxSetJointMaxForce(self.back_right_wheel, force, self.client.simxDefaultPublisher())
+        self.client.simxSetJointMaxForce(self.bl_wheel, force, self.client.simxDefaultPublisher())
+        self.client.simxSetJointMaxForce(self.br_wheel, force, self.client.simxDefaultPublisher())
 
     def set_velocity(self, velocity):
-        self.client.simxSetJointTargetVelocity(self.back_left_wheel, velocity, self.client.simxDefaultPublisher())
-        self.client.simxSetJointTargetVelocity(self.back_right_wheel, velocity, self.client.simxDefaultPublisher())
+        self.client.simxSetJointTargetVelocity(self.bl_wheel, velocity, self.client.simxDefaultPublisher())
+        self.client.simxSetJointTargetVelocity(self.br_wheel, velocity, self.client.simxDefaultPublisher())
 
     # Ustawia koła zgodnie z podanym promieniem skrętu. Kąt dodatni w prawo, ujemny w lewo
     def set_wheels_by_radius(self, radius):
@@ -37,10 +37,10 @@ class Car:
         else:
             left_angle, right_angle = -close_angle, -far_angle
 
-        self.client.simxSetJointTargetPosition(self.front_right_wheel, left_angle, self.client.simxDefaultPublisher())
-        self.client.simxSetJointTargetPosition(self.front_left_wheel, right_angle, self.client.simxDefaultPublisher())
+        self.client.simxSetJointTargetPosition(self.fr_wheel, left_angle, self.client.simxDefaultPublisher())
+        self.client.simxSetJointTargetPosition(self.fl_wheel, right_angle, self.client.simxDefaultPublisher())
 
-    # Ustawia koła pod podanym kątem (około). Kąt dodatni w prawo, ujemny w lewo
+    # Ustawia kół pod podanym kątem. Kąt dodatni w prawo, ujemny w lewo
     def set_wheels_by_angle(self, angle):
         radius = self.length / math.tan(util.deg2rad(angle)) if angle != 0 else math.inf
         self.set_wheels_by_radius(radius)
@@ -51,3 +51,28 @@ class Car:
         image = np.array(data, dtype=np.uint8)
         image.resize([size[1], size[0], 3])
         return image
+
+    def set_running_lights(self, enabled):
+        sig_value = 1 if enabled else 0
+        self.client.simxSetIntSignal("RunningLights", sig_value, self.client.simxServiceCall())
+
+    def set_stop_lights(self, enabled):
+        sig_value = 1 if enabled else 0
+        self.client.simxSetIntSignal("StopLights", sig_value, self.client.simxServiceCall())
+
+    def set_reversing_lights(self, enabled):
+        sig_value = 1 if enabled else 0
+        self.client.simxSetIntSignal("ReversingLights", sig_value, self.client.simxServiceCall())
+
+    def set_hazard_lights(self, enabled):
+        sig_value = 3 if enabled else 0
+        self.client.simxSetIntSignal("IndicatorsLights", sig_value, self.client.simxServiceCall())
+
+    def enable_left_indicators(self):
+        self.client.simxSetIntSignal("IndicatorsLights", 1, self.client.simxServiceCall())
+
+    def enable_right_indicators(self):
+        self.client.simxSetIntSignal("IndicatorsLights", 2, self.client.simxServiceCall())
+
+    def disable_indicators(self):
+        self.client.simxSetIntSignal("IndicatorsLights", 0, self.client.simxServiceCall())
--- app/controller.py
+++ app/controller.py
@@ -10,8 +10,7 @@ class Controller:
     def start(self):
         self.car.set_wheels_by_radius(0)
         self.car.set_force(60)
-        self.car.set_velocity(20)
-
+        self.car.set_running_lights(True)
         self.loop()
 
     def loop(self):
@@ -20,7 +19,39 @@ class Controller:
 
     def update(self):
         # Pijany kierowca
-        self.car.set_wheels_by_radius(2)
-        time.sleep(2)
-        self.car.set_wheels_by_radius(-2)
-        time.sleep(2)
+        self.car.set_velocity(10)
+
+        self.car.set_wheels_by_angle(0)
+        time.sleep(3)
+
+        self.car.set_wheels_by_radius(1)
+        self.car.enable_right_indicators()
+        time.sleep(3)
+
+        self.car.set_wheels_by_radius(-1)
+        self.car.enable_left_indicators()
+        time.sleep(3)
+
+        self.car.set_velocity(0)
+        self.car.set_wheels_by_angle(0)
+        self.car.disable_indicators()
+        self.car.set_stop_lights(True)
+        time.sleep(1)
+
+        self.car.set_stop_lights(False)
+        time.sleep(1)
+
+        self.car.set_reversing_lights(True)
+        self.car.set_velocity(-10)
+        time.sleep(3)
+
+        self.car.set_wheels_by_radius(1)
+        self.car.enable_right_indicators()
+        time.sleep(3)
+
+        self.car.set_wheels_by_radius(-1)
+        self.car.enable_left_indicators()
+        time.sleep(3)
+
+        self.car.disable_indicators()
+        self.car.set_reversing_lights(False)
