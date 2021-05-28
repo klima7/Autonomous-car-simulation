5d35d4e1d212ca503eccc83eac7f7c1310718b99
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-05-28 21:41:50 +0200

    AckermanSteering class removed

--- app/car.py
+++ app/car.py
@@ -30,62 +30,35 @@ class Lights:
         self.reverse = bool(vector[3])
 
 
-class AckermanSteering:
-
-    TURNING_ANGLE = math.pi / 3
-
-    def __init__(self):
-        self.velocity = 0
-        self.left_angle = 0
-        self.right_angle = 0
-
-    def get_vector(self):
-        return [self.velocity, self.left_angle, self.right_angle]
-
-    def set_vector(self, vector):
-        self.velocity = vector[0]
-        self.left_angle, self.right_angle = vector[1]
-
-    def set_wheels_by_radius(self, radius):
-        """positive radius - right, negative - left"""
-        close_angle = math.atan(Car.LENGTH / (abs(radius) - Car.WIDTH/2))
-        far_angle = math.atan(Car.LENGTH / (abs(radius) + Car.WIDTH / 2))
-
-        if radius < 0:
-            left_angle, right_angle = far_angle, close_angle
-        else:
-            left_angle, right_angle = -close_angle, -far_angle
-
-        self.right_angle = left_angle
-        self.left_angle = right_angle
-
-    def set_wheels_by_angle(self, angle):
-        """positive angle - right, negative - left"""
-        radius = Car.LENGTH / math.tan(util.deg2rad(angle)) if angle != 0 else math.inf
-        self.set_wheels_by_radius(radius)
-
-
 class Car:
     
     LENGTH = 0.316
     WIDTH = 0.213
+    TURNING_ANGLE = math.pi / 3
 
     def __init__(self, client: RemoteApiClient):
         self._client = client
+
+        self.velocity = 0
+        self.left_angle = 0
+        self.right_angle = 0
+
         _, self.camera_handle = self._client.simxGetObjectHandle('ViewCamera', self._client.simxServiceCall())
 
         self.gps = None
         self.orient = None
         self.view = None
 
-        self.steering = AckermanSteering()
         self.lights = Lights()
 
     def refresh(self):
         _, *data = self._client.simxCallScriptFunction("get_state@Car", "sim.scripttype_childscript", [], self._client.simxServiceCall())
         self.gps = Point(*data[0][0:2])
         self.orient = data[1]
-        self.steering.set_vector(data[2])
+
+        self.velocity = data[2][0]
+        self.left_angle, self.right_angle = data[2][1]
+
         self.lights.set_vector(data[3])
         self.view = self._get_camera_view()
 
@@ -98,7 +71,8 @@ class Car:
         return view
 
     def apply(self):
-        data = [self.steering.get_vector(), self.lights.get_vector()]
+        steering_data = [self.velocity, self.left_angle, self.right_angle]
+        data = [steering_data, self.lights.get_vector()]
         self._client.simxCallScriptFunction("set_state@Car", "sim.scripttype_childscript", data, self._client.simxServiceCall())
 
     def navigate(self, target: Point):
@@ -107,11 +81,27 @@ class Car:
         car_angle = self.orient
         diff_angle = car_angle - target_angle
 
-        diff_angle = min(self.steering.TURNING_ANGLE, diff_angle)
-        diff_angle = max(-self.steering.TURNING_ANGLE, diff_angle)
+        diff_angle = min(self.TURNING_ANGLE, diff_angle)
+        diff_angle = max(-self.TURNING_ANGLE, diff_angle)
 
         diff_angle = util.rad2deg(diff_angle)
-        self.steering.set_wheels_by_angle(diff_angle)
+        self.set_wheels_by_angle(diff_angle)
+
+    def set_wheels_by_radius(self, radius):
+        close_angle = math.atan(Car.LENGTH / (abs(radius) - Car.WIDTH/2))
+        far_angle = math.atan(Car.LENGTH / (abs(radius) + Car.WIDTH / 2))
+
+        if radius < 0:
+            left_angle, right_angle = far_angle, close_angle
+        else:
+            left_angle, right_angle = -close_angle, -far_angle
+
+        self.right_angle = left_angle
+        self.left_angle = right_angle
+
+    def set_wheels_by_angle(self, angle):
+        radius = Car.LENGTH / math.tan(util.deg2rad(angle)) if angle != 0 else math.inf
+        self.set_wheels_by_radius(radius)
 
     @property
     def preview_point(self):
