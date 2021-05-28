2e61d8e31ce5979ebaee72ed94261d95161e77d9
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-05-28 22:10:25 +0200

    Unused simulation module removed May be reverted in the future

--- app/starter.py
+++ app/starter.py
@@ -1,6 +1,5 @@
 from api import b0RemoteApi
 from car import Car
-from simulation import Simulation
 from controller import Controller
 from meta import MetaManager
 
@@ -11,6 +10,5 @@ CHANNEL_NAME = 'b0RemoteApi'
 with b0RemoteApi.RemoteApiClient(CLIENT_NAME, CHANNEL_NAME, inactivityToleranceInSec=60, timeout=10) as client:
     car = Car(client)
     mm = MetaManager(client)
-    simulation = Simulation(client)
     controller = Controller(car, mm)
     controller.start()
