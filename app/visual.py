ee1fdbf5b0a11256172b69a8c4c022659fb7cffd
klima7 <ukasz.klimkiewicz2@wp.pl>
2021-06-14 13:40:24 +0200

    Finding signs cleanup

--- app/visual.py
+++ app/visual.py
@@ -12,24 +12,70 @@ CAMERA_IMAGE_WIDTH = 512
 SIGN_REAL_HEIGHT = 0.48978
 
 
+class FoundSign:
+
+    def __init__(self, stick, image):
+        self.stick = stick
+        self.head = self.find_sign_head()
+        self.head_image = self.cut_head_image(image)
+        self.type = None
+
+    def find_sign_head(self):
+        x, y, h = self.stick
+        head_size = int(SIGN_HEAD2STICK_FACTOR * h)
+        return x - head_size // 2, y - head_size, head_size
+
+    def cut_head_image(self, image):
+        x, y, a = self.head
+        if x < 0 or y < 0 or x + a >= CAMERA_IMAGE_WIDTH or y + a >= CAMERA_IMAGE_HEIGHT:
+            return None
+        return image[y:y + a, x:x + a]
+
+    def get_distance(self):
+        height = self.stick[2]
+        numerator = CAMERA_IMAGE_HEIGHT * SIGN_REAL_HEIGHT
+        denominator = 2 * height * math.tan(util.deg2rad(PERSPECTIVE_ANGLE/2))
+        return numerator / denominator
+
+    def get_angle(self):
+        x = self.stick[0]
+        diff = x - CAMERA_IMAGE_WIDTH / 2
+        a_diff = abs(diff)
+        a_angle = a_diff * util.deg2rad(PERSPECTIVE_ANGLE/2) / (CAMERA_IMAGE_WIDTH / 2)
+        angle = a_angle if diff > 0 else -a_angle
+        return angle
+
+    def draw(self, hsv_image):
+        head_start = (self.head[0], self.head[1])
+        head_end = (self.head[0]+self.head[2], self.head[1]+self.head[2])
+        cv2.rectangle(hsv_image, head_start, head_end, (0, 0, 0), 2)
+
+        stick_start = (self.stick[0], self.stick[1])
+        stick_end = (self.stick[0], self.stick[1]+self.stick[2])
+        cv2.line(hsv_image, stick_start, stick_end, (0, 0, 0), 2)
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(hsv_image, 'Sign', (head_start[0]-50, head_start[1]), font, 1, (0, 0, 0), 1, cv2.LINE_AA)
+
+
 def find_signs(image):
     if image is None:
         return
 
     hsv_image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    sticks = find_signs_sticks(hsv_image)
-    heads = [find_sign_head(stick) for stick in sticks]
-    draw_rectangles(hsv_image, heads)
 
+    sticks = find_sticks(hsv_image)
+    signs = [FoundSign(stick, hsv_image) for stick in sticks]
+
+    for sign in signs:
+        sign.draw(hsv_image)
     cv2.imshow('Frame', cv2.cvtColor(hsv_image, cv2.COLOR_HSV2BGR))
     cv2.waitKey(1)
 
 
-def find_signs_sticks(hsv_image):
+def find_sticks(hsv_image):
     sticks = []
-
     mask = cv2.inRange(hsv_image, np.array([0, 0, 70]), np.array([3, 3, 85]))
-
     contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(hsv_image, contours, -1, (0, 0, 255), 1)
     for i in range(len(contours)):
@@ -37,36 +83,7 @@ def find_signs_sticks(hsv_image):
         x, y, w, h = cv2.boundingRect(contour)
         if h > 15:
             sticks.append((x+w//2, y, h))
-
     return sticks
-
-
-def find_sign_head(stick):
-    x, y, h = stick
-    print(calc_distance(h), util.rad2deg(calc_angle(x)))
-    head_size = int(SIGN_HEAD2STICK_FACTOR * h)
-    return x - head_size//2, y - head_size, head_size, head_size
-
-
-def calc_distance(height):
-    numerator = CAMERA_IMAGE_HEIGHT * SIGN_REAL_HEIGHT
-    denominator = 2 * height * math.tan(util.deg2rad(PERSPECTIVE_ANGLE/2))
-    return numerator / denominator
-
-
-def calc_angle(x):
-    diff = x - CAMERA_IMAGE_WIDTH / 2
-    a_diff = abs(diff)
-    a_angle = a_diff * util.deg2rad(PERSPECTIVE_ANGLE/2) / (CAMERA_IMAGE_WIDTH / 2)
-    angle = a_angle if diff > 0 else -a_angle
-    return angle
-
-
-def draw_rectangles(image, rectangles):
-    for rectangle in rectangles:
-        start = (rectangle[0], rectangle[1])
-        end = (rectangle[0]+rectangle[2], rectangle[1]+rectangle[3])
-        cv2.rectangle(image, start, end, (0, 0, 0), 2)
 
 
 class TrafficLightColor(Enum):
